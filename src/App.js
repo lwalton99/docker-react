4f786b2f83a8f1db04d4049146100cc93898450d
Larry Walton <lwalton99@larrywatonsmbp2.lan>
2021-01-17 09:20:26 -0600

    update 2 Jan

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!  Very cool updates January
+          Hi there!  Very cool updates January 2
         </p>
         <a
           className="App-link"
